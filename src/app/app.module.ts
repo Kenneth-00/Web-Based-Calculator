29023e49b01da1403802fbd91ed4b4a69ddbddae
Kenneth <palencia.kenneth69@gmail.com>
2022-01-06 09:34:51 -0800

    add calculator component and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './component/calculator/calculator.component';
 
+const routes: Routes = [
+  {
+    path:"myCalculator", component: CalculatorComponent,
+    children: [
+      {path:"calculator", component:CalculatorComponent},
+    ],
+  },
+  {path:"", redirectTo:"myCalculator", pathMatch:"full"}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
